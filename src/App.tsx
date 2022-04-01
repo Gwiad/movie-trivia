39ac347eb518fe4133b343845d61ae5d0acdfcda
Gwiad <53998994+Gwiad@users.noreply.github.com>
2022-04-01 11:42:50 +0200

    router

--- src/App.tsx
+++ src/App.tsx
@@ -1,56 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Route, Routes} from 'react-router';
+import {BrowserRouter} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/">homepage</Route>
+          <Route path="/play/:questionId">play here</Route>
+          <Route path="/gameOver">game over </Route>
+          <Route path="/error">error</Route>
+          <Route>lost in 404</Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
-
 export default App;
