9777691b7c95ed5f7cab3dc94bfe47dd87d0d4dd
Gwiad <53998994+Gwiad@users.noreply.github.com>
2022-04-01 11:42:50 +0200

    use local storage

--- src/features/movies/movies.slice.ts
+++ src/features/movies/movies.slice.ts
@@ -1,4 +1,6 @@
 import {call, put, takeEvery} from '@redux-saga/core/effects';
+import {GENERATE_QUESTIONS} from 'features/questions/questions.slice';
+import {TakeableChannel} from 'redux-saga';
 import {Movie} from 'types';
 
 const key = process.env.REACT_APP_IMDB_PUBLIC_API_KEY;
@@ -49,21 +51,45 @@ export default function moviesReducer(state = defaultMovieState, action: MoviesA
 
 export function* onFetchMovies(action: typeof FETCH_MOVIES) {
   try {
-    // need to set types
-    const response = yield call(fetch, url);
-    const json = yield call([response, 'json']);
-    if (json.items.length < 1 && json.errorMessage.length > 0) {
-      yield put({type: RECEIVE_MOVIES_ERROR, payload: json.errorMessage});
+    const moviesCache = localStorage.getItem('movies');
+    const moviesCacheParsed = moviesCache ? JSON.parse(moviesCache) : null;
+    let response;
+    let json;
+    if (!moviesCacheParsed) {
+      // need to set types
+      response = yield call(fetch, url);
+      json = yield call([response, 'json']);
+      if (json.items.length < 1 && json.errorMessage.length > 0) {
+        yield put({type: RECEIVE_MOVIES_ERROR, payload: json.errorMessage});
+      } else {
+        localStorage.setItem('movies', JSON.stringify(json.items));
+        yield put({type: RECEIVE_MOVIES_SUCCESS, payload: json.items});
+      }
     } else {
-      yield put({type: RECEIVE_MOVIES_SUCCESS, payload: json.items});
+      yield put({type: RECEIVE_MOVIES_SUCCESS, payload: moviesCacheParsed});
     }
   } catch (error) {
     // need to set types
     yield put({type: RECEIVE_MOVIES_ERROR, payload: error.toString()});
   }
 }
+export function* onReceiveMoviesSuccess({
+  action,
+  payload,
+}: {
+  action: typeof RECEIVE_MOVIES_SUCCESS;
+  payload: Movie[];
+}) {
+  yield put({
+    type: GENERATE_QUESTIONS,
+    payload: payload,
+  });
+}
 
 export function* moviesSaga() {
-  // @ts-expect-error : need to set types
-  yield takeEvery(FETCH_MOVIES, onFetchMovies);
+  yield takeEvery(FETCH_MOVIES as unknown as TakeableChannel<unknown>, onFetchMovies);
+  yield takeEvery(
+    RECEIVE_MOVIES_SUCCESS as unknown as TakeableChannel<unknown>,
+    onReceiveMoviesSuccess
+  );
 }
